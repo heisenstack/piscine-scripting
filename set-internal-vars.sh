08cc329c23b77cc5056ca7007a4fced35cef44c1
heisenstack <dinaniamine@gmail.com>
2026-03-31 14:08:32 +0100

    set internal vars

--- set-internal-vars.sh
+++ set-internal-vars.sh
@@ -3,7 +3,7 @@ MY_NUM=100
 MY_PI=3.142
 MY_ARR=(one two three four five)
 
-echo "$MY_MESSAGE"
-echo "$MY_NUM"
-echo "$MY_PI"
-echo "${MY_ARR[*]}" | tr ' ' '\n' | tr '\n' ',' | sed 's/,/, /g' | sed 's/, $/\n/'
+# echo "$MY_MESSAGE"
+# echo "$MY_NUM"
+# echo "$MY_PI"
+# echo "${MY_ARR[*]}" | tr ' ' '\n' | tr '\n' ',' | sed 's/,/, /g' | sed 's/, $/\n/'
