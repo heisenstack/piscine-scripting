bb8f18efea777bd4d79364a660b9da79626d6373
Mohamed Amine Dinani <mdinani@noreply.learn.zone01oujda.ma>
2026-04-05 16:27:23 +0000

    Update remake.sh

--- remake.sh
+++ remake.sh
@@ -1,30 +1,23 @@
 #!/usr/bin/env bash
 
-if [[ $# -ne 1 ]] || ! [[ -d "$1" ]]; then
+if [[ $# -ne 1 ]] || [ ! -d "$1" ]; then
     echo "Error"
     exit 1
+else
+    # echo "Directory $1 created"
+    touch -d "01 Jan 2026 00:01:00" $1/ciao 
+    mkdir $1/mamma
+    touch -d "02 Jan 2026 00:01:00" $1/mamma 
+    touch -d "03 Jan 2026 00:01:00" $1/guarda 
+    touch -d "04 Jan 2026 00:01:00" $1/come 
+    mkdir $1/mi
+    touch -d "05 Jan 2026 00:01:00" $1/mi 
+    touch -d "06 Jan 2026 00:01:00" $1/diverto
+
+    chmod 442 $1/ciao
+    chmod 777 $1/mamma
+    chmod 400 $1/guarda
+    chmod 642 $1/come
+    chmod 452 $1/mi
+    chmod 421 $1/diverto
 fi
-
-DIR="$1"
-
-touch "$DIR/ciao"
-touch "$DIR/guarda"
-touch "$DIR/come"
-touch "$DIR/diverto"
-
-mkdir -p "$DIR/mamma"
-mkdir -p "$DIR/mi"
-
-chmod 442 "$DIR/ciao"
-chmod 400 "$DIR/guarda"
-chmod 642 "$DIR/come"
-chmod 421 "$DIR/diverto"
-chmod 777 "$DIR/mamma"
-chmod 452 "$DIR/mi"
-
-touch -t 200101010001 "$DIR/ciao"
-touch -t 200201020001 "$DIR/mamma"
-touch -t 200301030001 "$DIR/guarda"
-touch -t 200401040001 "$DIR/come"
-touch -t 200501050001 "$DIR/mi"
-touch -t 200601060001 "$DIR/diverto"
