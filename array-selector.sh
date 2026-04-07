08b5ff819b2d5627858a99d5a4b635a00dc64aab
heisenstack <dinaniamine@gmail.com>
2026-04-07 09:05:47 +0100

    array selector

--- array-selector.sh
+++ array-selector.sh
@@ -4,12 +4,12 @@ array=("red" "blue" "green" "white" "black")
 
 if [[ $# -ne 1 ]] || ! [[ "$1" =~ ^[0-9]+$ ]]; then
     echo "Error"
-    exit 1
+    exit 0
 fi
 
 if [[ "$1" -lt 1 ]] || [[ "$1" -gt ${#array[@]} ]]; then
     echo "Error"
-    exit 1
+    exit 0
 fi
 
 
