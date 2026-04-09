7349130ee61fd22e5d4774109ecc26876ad45c9f
heisenstack <dinaniamine@gmail.com>
2026-04-09 12:48:50 +0100

    flex function

--- string_tokenizer_count.py
+++
@@ -1,8 +0,0 @@
-import re
-from collections import Counter
-
-def tokenizer_counter(string):
-    cleaned = re.sub(r'[^a-zA-Z0-9\s]', '', string).lower()
-    words = cleaned.split()
-    count = Counter(words)
-    return dict(sorted(count.items()))
